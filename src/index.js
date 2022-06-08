a080638979e7a0160bcbab0c45f7f344266c1557
Claire Thompson <clairethompson@Claires-MacBook-Pro.local>
2022-06-08 14:27:02 -0400

    created board squares and passed props

--- src/index.js
+++ src/index.js
@@ -16,25 +16,40 @@ import './index.css';
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
 
-const Square = () => {
-  return ( <div style={{
-    backgroundColor: 'gold',
-    margin: 10,
-    padding:20,
-  }}>
-    Square
+const Square = (props) => {
+  return ( <div className='square'>
+    {props.value}
   </div> );
 }
  
 
 const Board = () => {
+  const renderSquare = (i) => {
+    return ( <Square value={i} /> );
+  }
+  
   return ( <div style={{
     backgroundColor: 'skyblue',
     margin: 10,
     padding: 20,
   }}>
     Board
-  <Square />
+    <div className='board-row'>
+      {renderSquare(0)}
+      {renderSquare(1)}
+      {renderSquare(2)}
+    </div>
+    <div className='board-row'>
+      {renderSquare(3)}
+      {renderSquare(4)}
+      {renderSquare(5)}
+    </div>
+    <div className='board-row'>
+      {renderSquare(6)}
+      {renderSquare(7)}
+      {renderSquare(8)}
+    </div>
+
 
   </div> );
 }
@@ -42,10 +57,7 @@ const Board = () => {
 // export default Board;
 
 const Game = () => {
-  return ( <div style={{
-  backgroundColor: 'salmon',
-  margin: 10,
-  padding: 20,}} >
+  return ( <div className='game' >
     Game
   <Board />
   </div> );
